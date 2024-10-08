710d907da9c3681e208761ddc54c114e64b6f794
Yeonji07 <121606323+Yeonji07@users.noreply.github.com>
2024-10-08 16:49:35 +0700

    Updated vite.config.js with build configuration

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,26 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue(), vueJsx()],
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+
+    // manifest: true,
+    // rollupOptions: {
+    //   input: {
+    //     main: path.resolve(__dirname, 'index.html'),
+    //   },
+    // },
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
